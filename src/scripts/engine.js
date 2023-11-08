4c543a103bc841236a09a9398ed929437f3a6546
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-08 14:13:13 -0300

    feat: Add cards data

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,10 +1,33 @@
+const imagePath = "../assets/icons/";
+
+const cardsData = [
+  {
+    id: 0,
+    img: `${imagePath}exodia.png`,
+    name: "Exodia, O Proibido",
+    attribute: "str",
+  },
+  {
+    id: 1,
+    img: `${imagePath}dragon.png`,
+    name: "Dragão Branco de Olhos Azuis",
+    attribute: "dex",
+  },
+  {
+    id: 2,
+    img: `${imagePath}magician.png`,
+    name: "Mago Negro",
+    attribute: "int",
+  },
+];
+
 const state = {
   view: {
     scoreBox: document.getElementById("score-box"),
     card: {
       image: document.getElementById("card-image"),
       name: document.getElementById("card-name"),
-      status: document.getElementById("card-status"),
+      attribute: document.getElementById("card-attribute"),
     },
     fieldCards: {
       player: document.getElementById("player-card-field"),
