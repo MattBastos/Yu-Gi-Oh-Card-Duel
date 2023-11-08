ac203a156bd024be43ec62554035cb8957ec786c
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-08 16:59:13 -0300

    refactor: Removes determineWinner function

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,21 +3,21 @@ const imagePath = "../assets/icons/";
 const cardsData = [
   {
     id: 0,
-    img: `${imagePath}exodia.png`,
     name: "Exodia, O Proibido",
-    attribute: "str",
+    attribute: "rock",
+    img: `${imagePath}exodia.png`,
   },
   {
     id: 1,
-    img: `${imagePath}dragon.png`,
     name: "Dragão Branco de Olhos Azuis",
-    attribute: "dex",
+    attribute: "scissors",
+    img: `${imagePath}dragon.png`,
   },
   {
     id: 2,
-    img: `${imagePath}magician.png`,
     name: "Mago Negro",
-    attribute: "int",
+    attribute: "paper",
+    img: `${imagePath}magician.png`,
   },
 ];
 
@@ -48,26 +48,8 @@ const state = {
 
 const { view, values } = state;
 
-const determineWinner = (playerCard, enemyCard) => {
-  const playerCardAtt = cardsData.find((card) => card === playerCard).attribute;
-  const enemyCardAtt = cardsData.find((card) => card === enemyCard).attribute;
-  const { attributes } = values;
-
-  if (attributes[playerCardAtt].beats === enemyCardAtt) {
-    return "O Jogador Venceu!";
-  } else if (attributes[playerCardAtt].losesTo === enemyCardAtt) {
-    return "O Inimigo Venceu!";
-  } else {
-    return "Empate!";
-  }
-};
-
 const init = () => {
-  const playerCard = cardsData[1];
-  const enemyCard = cardsData[2];
-
-  const duelResult = determineWinner(playerCard, enemyCard);
-  console.log(duelResult);
+  drawCards();
 };
 
 init();
